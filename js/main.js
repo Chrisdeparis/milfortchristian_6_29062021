b291aa1bcd7747e21ae38d9492fccc4fd09ceb7b
maescristro <milfortchristian@gmail.com>
2021-07-05 22:20:32 +0200

    routes

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ const fetchData = () => {
             console.log(dash);
           return `        
             <div class="user ">
-                <a href="/${dash}.html">
+                <a href="milfortchristian_6_29062021/${dash}.html">
                 <div class="circle thumb">
                     <div class="crop">
                     <img src="img/${user.portrait}" alt="" />
