2755585059ebd5f22cbeafb969350874f80e1a39
maescristro <milfortchristian@gmail.com>
2021-09-20 17:13:36 +0200

    show image lightbox onclick

--- js/modules/lightBox.js
+++ js/modules/lightBox.js
@@ -2,9 +2,11 @@
 console.log("lightbox test");
 import { media } from "./media.js";
 import data from "../FishEyeData.js";
+// import {listPictures} from './media.js'
 
 // Lorsque l'utilisateur clique sur un média, celui-ci doit s’ouvrir dans une
 // lightbox :
+
 const openLightBox = () => {
   const url_string = window.location.href;
   console.log(url_string);
@@ -22,23 +24,32 @@ const openLightBox = () => {
       return pathuser;
     }
   });
-
-  for (let i = 0; i < thumbImg.length; i++) {
-    // Get the modal
-    const modal = document.getElementById("openLightBox");
-
-    // Get the <span> element that closes the modal
-    const span = document.getElementsByClassName("close")[0];
-    // Get the thumb img that opens the modal
-    thumbImg[i].addEventListener("click", (e) => {
-      console.log("open lightbox n° " + i+"media id= "+media.id);
+  console.log(thumbImg);
+  let pathImg;
+  for(let i= 0; i<thumbImg.length; i++){
+    thumbImg[i].addEventListener('click', function(){
+      console.log(thumbImg[i]);
+      // Get the modal
+      const modal = document.getElementById("openLightBox");
+      // Get the <span> element that closes the modal
+      const span = document.getElementsByClassName("close")[0];
+      let path = user[0].name.split(" ")[0];
+      let boxImage = [];
+      // let listPictures = media();
+     
+      pathImg = thumbImg[i].src.slice(21);
       
-      let boxImage = data.media;
+      
+      // 
+      console.log(pathImg);
       modal.style.display = "block";
+      document.getElementById("boxImage").src = pathImg;
+      // document.getElementById("title").innerHTML = altTitle;
 
       // When the user clicks on <span> (x), close the modal
       span.onclick = function () {
         modal.style.display = "none";
+        
       };
       // When the user clicks anywhere outside of the modal, close it
       window.onclick = function (event) {
@@ -46,37 +57,30 @@ const openLightBox = () => {
           modal.style.display = "none";
         }
       };
-      let altTitle;
-      
-      
-      // data.media.filter((media) => {
-      //   // console.log(media);
-        
-      //   if (media.photographerId == myparam) {
-      //     if (media.image) {
-      //       // console.log(media);
-      //       let path = user[0].name.split(" ")[0];
-      //       altTitle = media.title;
-      //       boxImage = "/img/Photos/"+path+'/'+media.image+'';
-      //       //when the user click on left chevron
-      //       const chevronLeft = document.getElementById("chevronLeft");
-      //       chevronLeft.onclick = function (event) {
-      //         console.log("previous image");
-      //         console.log(media); 
+      let altTitle = "";
 
-      //       };
-      //       // when the user click on right chevron
-      //       const chevronRight = document.getElementById("chevronRight");
-      //       chevronRight.onclick = function (event) {
-      //         console.log("next image");
-      //       };
-      //     }
-      //   }
-      // });
-      // document.getElementById('boxImage').src = boxImage;
-      // document.getElementById("boxImage").alt = altTitle;
-    });
+      let pictureId = "";
+      //when the user click on left chevron
+      const chevronLeft = document.getElementById("chevronLeft");
+      chevronLeft.onclick = function (event) {
+        console.log("previous image");
+        // console.log(media);
+      };
+      // when the user click on right chevron
+      const chevronRight = document.getElementById("chevronRight");
+      chevronRight.onclick = function (event) {
+        console.log("next image");
+      };
+
+    })
+    // console.log(pathImg);
+    // document.getElementById("boxImage").id = pictureId;
+    // 
+    // document.getElementById('boxImage').alt =  altTitle;
+    // 
+
   }
-};
+}
+
 
 openLightBox();
--- js/modules/media.js
+++ js/modules/media.js
@@ -1,4 +1,5 @@
 import data from "../FishEyeData.js";
+// import { myListPictures } from './media';
 
 const media = () => {
     const url_string = window.location.href;
@@ -22,7 +23,7 @@ const media = () => {
     
     let res = ""; 
     let totalLikes = 0;
-    let mediaId= "124145";
+    const listPictures = [];
     data.media.filter((media) => {
         if(media.photographerId == myparam){
             let path = user[0].name.split(' ')[0];
@@ -31,17 +32,27 @@ const media = () => {
                 totalLikes += parseFloat(media.likes);
             }
             if(media.image) {
+                
+                console.log(listPictures.push(media.image));
+                
                 res += '<div><img id="'+media.id+'" class="thumb-img" alt="'+media.title+'" src="/img/Photos/'+path+'/'+media.image+'"/><div class="thumb-info"><p>'+media.title+'</p><div class="thumb-likes"><div class="likes"><p>'+media.likes+'</p></div><img class="heart" src="../img/heart-solid.svg" alt="likes"/></div></div></div>';
                 
             }      
         }
     })    
-    // document.getElementById('media-id').id = mediaId;
+    console.log(listPictures);
+    
+    // export listPictures;
+    // export let listPictures;
+    console.log(listPictures.length);
+    
     document.getElementById('likes').innerHTML = totalLikes;
     document.getElementById('photos-section').innerHTML = res;
+    return listPictures;
     
     
 
 }
 media();
+
 export { media };
