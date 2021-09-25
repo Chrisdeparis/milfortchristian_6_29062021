e1de24c4aaea1370a99cc4b05a96fce5600ddb70
maescristro <milfortchristian@gmail.com>
2021-09-25 15:43:09 +0200

    add gallery img and videos

--- js/modules/lightBox.js
+++ js/modules/lightBox.js
@@ -1,6 +1,6 @@
 import { media } from "./media.js";
 import data from "../FishEyeData.js";
-import { likes } from './likes.js';
+import { likes } from "./likes.js";
 
 // Lorsque l'utilisateur clique sur un média, celui-ci doit s’ouvrir dans une
 // lightbox :
@@ -22,7 +22,7 @@ const openLightBox = () => {
 
   //gallery
   let galleryImages = document.querySelectorAll(".gallery-img");
-  
+
   likes();
   if (galleryImages) {
     galleryImages.forEach(function (image) {
@@ -30,6 +30,7 @@ const openLightBox = () => {
       image.addEventListener("click", function (e) {
         //determine the image clicked
         let imageClicked = e.target.closest("div img");
+        let videoClicked = e.target.closest("div video");
 
         // Get the modal
         const modal = document.getElementById("openLightBox");
@@ -45,91 +46,222 @@ const openLightBox = () => {
         let html = document.getElementById("html");
         html.classList.add("scroll");
 
-        // document.getElementById("boxImage").src = imageClicked.src;
-        document.getElementById("title").innerHTML = imageClicked.alt;
+        if (imageClicked) {
+          document.getElementById("boxVideo").style.display = "none";
+          document.getElementById("boxImage").src = imageClicked.src;
+          document.getElementById("title").innerHTML = imageClicked.alt;
 
-        //recuperer data-p
-        let actual = imageClicked.getAttribute("data-p");
+          //recuperer data-p
+          let actual = imageClicked.getAttribute("data-p");
+
+          //when the user click on left chevron previous element
+          const chevronLeft = document.getElementById("chevronLeft");
+
+          chevronLeft.onclick = function (event) {
+            document.getElementById("boxVideo").style.display = "none";
+            //la liste des medias
+            let medias = JSON.parse(
+              document.querySelector("#photoList").getAttribute("data-list")
+            );
+            //la liste des alt pour titre
+            let altList = JSON.parse(
+              document.querySelector("#photoList").getAttribute("alt-list")
+            );
+
+            let images = thumbImg;
+            let limit = images.length - 1;
+
+            function mySlide(param) {
+              if (param === "prev") {
+                if (actual == 0) {
+                  actual = limit;
+                } else {
+                  actual--;
+                }
+              } else {
+                if (actual == limit) {
+                  actual = 0;
+                } else {
+                  actual++;
+                }
+              }
+
+              //if src = .mp4
+              let mp4Video = medias[actual].includes(".mp4");
+
+              if (mp4Video) {
+                document.getElementById("boxImage").style.display = "none";
+                document.getElementById('boxVideo').style.display = "block";
+                document.getElementById("boxVideo").src =
+                  "/img/Photos/" + path + "/" + medias[actual] + "";
+                
+              } else {
+                document.getElementById("boxImage").style.display = "block";
+                document.getElementById("boxVideo").style.display = "none";
+              }
+
+              document.getElementById("boxImage").src =
+                "/img/Photos/" + path + "/" + medias[actual] + "";
+              document.getElementById("title").innerHTML = altList[actual];
+            }
+
+            mySlide("prev");
+          };
+
+          //when the user click on right chevron next element
+          const chevronRight = document.getElementById("chevronRight");
+
+          chevronRight.onclick = function (event) {
+            document.getElementById("boxVideo").style.display = "none";
+            let medias = JSON.parse(
+              document.querySelector("#photoList").getAttribute("data-list")
+            );
+            let altList = JSON.parse(
+              document.querySelector("#photoList").getAttribute("alt-list")
+            );
+
+            let i = 0,
+              images = thumbImg;
+            let limit = images.length - 1;
+
+            function mySlide(param) {
+              if (param === "next") {
+                if (actual == 0) {
+                  actual = limit;
+                } else {
+                  actual--;
+                }
+              } else {
+                if (actual == limit) {
+                  actual = 0;
+                } else {
+                  actual++;
+                }
+              }
+
+              //if src = .mp4
+              let mp4Video = medias[actual].includes(".mp4");
+
+              if (mp4Video) {
+                document.getElementById("boxImage").style.display = "none";
+                document.getElementById('boxVideo').style.display = "block";
+                document.getElementById("boxVideo").src =
+                  "/img/Photos/" + path + "/" + medias[actual] + "";
+                
+              } else {
+                document.getElementById("boxImage").style.display = "block";
+                document.getElementById("boxVideo").style.display = "none";
+              }
+
+              document.getElementById("boxImage").src =
+                "/img/Photos/" + path + "/" + medias[actual] + "";
+              document.getElementById("title").innerHTML = altList[actual];
+            }
+
+            mySlide("next");
+          };
+        }
+
+        if (videoClicked) {
+          document.getElementById("boxVideo").style.display = "block";
+          document.getElementById("boxImage").style.display = "none";
+
+          //recuperer data-p
+          let actual = videoClicked.getAttribute("data-p");
+
+          let altList = JSON.parse(
+            document.querySelector("#photoList").getAttribute("alt-list")
+          );
+          document.getElementById("boxVideo").src = videoClicked.src;
+          document.getElementById("title").innerHTML = altList[actual];
+
+          //when the user click on left chevron previous element
+          const chevronLeft = document.getElementById("chevronLeft");
+
+          chevronLeft.onclick = function (event) {
+            document.getElementById("boxImage").style.display = "block";
+            document.getElementById("boxVideo").style.display = "none";
+
+            let medias = JSON.parse(
+              document.querySelector("#photoList").getAttribute("data-list")
+            );
+            let altList = JSON.parse(
+              document.querySelector("#photoList").getAttribute("alt-list")
+            );
+
+            let images = thumbImg;
+            let limit = images.length - 1;
+
+            function mySlide(param) {
+              if (param === "prev") {
+                if (actual == 0) {
+                  actual = limit;
+                } else {
+                  actual--;
+                }
+              } else {
+                if (actual == limit) {
+                  actual = 0;
+                } else {
+                  actual++;
+                }
+              }
+              console.log(medias);
+              document.getElementById("boxImage").src =
+                "/img/Photos/" + path + "/" + medias[actual] + "";
+              document.getElementById("title").innerHTML = altList[actual];
+            }
+
+            mySlide("prev");
+          };
+
+          //when the user click on right chevron next element
+          const chevronRight = document.getElementById("chevronRight");
+
+          chevronRight.onclick = function (event) {
+            document.getElementById("boxImage").style.display = "block";
+            document.getElementById("boxVideo").style.display = "none";
+
+            let medias = JSON.parse(
+              document.querySelector("#photoList").getAttribute("data-list")
+            );
+            let altList = JSON.parse(
+              document.querySelector("#photoList").getAttribute("alt-list")
+            );
+
+            let i = 0,
+              images = thumbImg;
+            let limit = images.length - 1;
+
+            function mySlide(param) {
+              if (param === "next") {
+                if (actual == 0) {
+                  actual = limit;
+                } else {
+                  actual--;
+                }
+              } else {
+                if (actual == limit) {
+                  actual = 0;
+                } else {
+                  actual++;
+                }
+              }
+
+              document.getElementById("boxImage").src =
+                "/img/Photos/" + path + "/" + medias[actual] + "";
+              document.getElementById("title").innerHTML = altList[actual];
+            }
+
+            mySlide("next");
+          };
+        }
+
         // // When the user clicks on <span> (x), close the modal
         span.onclick = function () {
           modal.style.display = "none";
           let html = document.getElementById("html");
           html.classList.remove("scroll");
-        };
-
-        //when the user click on left chevron previous element
-        const chevronLeft = document.getElementById("chevronLeft");
-
-        chevronLeft.onclick = function (event) {
-          let image2 = JSON.parse(
-            document.querySelector("#photoList").getAttribute("data-list")
-          );
-          let altList = JSON.parse(
-            document.querySelector("#photoList").getAttribute("alt-list")
-          );
-
-          let images = thumbImg;
-          let limit = images.length - 1;
-
-          function mySlide(param) {
-            if (param === "prev") {
-              if (actual == 0) {
-                actual = limit;
-              } else {
-                actual--;
-              }
-            } else {
-              if (actual == limit) {
-                actual = 0;
-              } else {
-                actual++;
-              }
-            }
-            console.log(image2);
-            document.getElementById("boxImage").src =
-              "/img/Photos/" + path + "/" + image2[actual] + "";
-            document.getElementById("title").innerHTML = altList[actual];
-          }
-
-          mySlide("prev");
-        };
-
-        //when the user click on right chevron next element
-        const chevronRight = document.getElementById("chevronRight");
-
-        chevronRight.onclick = function (event) {
-          let image2 = JSON.parse(
-            document.querySelector("#photoList").getAttribute("data-list")
-          );
-          let altList = JSON.parse(
-            document.querySelector("#photoList").getAttribute("alt-list")
-          );
-
-          let i = 0,
-            images = thumbImg;
-          let limit = images.length - 1;
-
-          function mySlide(param) {
-            if (param === "next") {
-              if (actual == 0) {
-                actual = limit;
-              } else {
-                actual--;
-              }
-            } else {
-              if (actual == limit) {
-                actual = 0;
-              } else {
-                actual++;
-              }
-            }
-            
-            document.getElementById("boxImage").src =
-              "/img/Photos/" + path + "/" + image2[actual] + "";
-            document.getElementById("title").innerHTML = altList[actual];
-          }
-
-          mySlide("next");
         };
       });
     });
